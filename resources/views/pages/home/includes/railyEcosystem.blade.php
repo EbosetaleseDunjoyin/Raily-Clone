a05c7cf171e78b7ec730c3baade466c3745674ca
EbosetaleseDunjoyin <eboselese@gmail.com>
2024-06-10 23:11:21 +0100

    rally section

--- resources/views/pages/home/includes/railyEcosystem.blade.php
+++ resources/views/pages/home/includes/railyEcosystem.blade.php
@@ -31,14 +31,41 @@
          Concierge
        </button>
     </div>
-    <div class="block-mach-absolut">
-    <div class="teg-mach-ar pl-absolut">
-    <div class="text-29">AR</div>
-    <img src="https://cdn.prod.website-files.com/64aceaf9cb10fc1c8e4efdbc/66320bec42874fc2c379d8f1_Group%201948758571.svg" loading="lazy" alt="" class="icon-teg">
+    <div class="block-mach-absolut first">
+        <div class="teg-mach-ar pl-absolut">
+        <div class="text-29">AR</div>
+        <img src="https://cdn.prod.website-files.com/64aceaf9cb10fc1c8e4efdbc/66320bec42874fc2c379d8f1_Group%201948758571.svg" loading="lazy" alt="" class="icon-teg">
+        </div>
+        <div class="box-im-mach">
+        <img src="https://cdn.prod.website-files.com/64aceaf9cb10fc1c8e4efdbc/66320b3bdbdcfd73b6efc8dd_Group%201948758718%20(1).webp" loading="lazy" alt="" class="in-mach">
+        </div>
     </div>
-    <div class="box-im-mach">
-    <img src="https://cdn.prod.website-files.com/64aceaf9cb10fc1c8e4efdbc/66320b3bdbdcfd73b6efc8dd_Group%201948758718%20(1).webp" loading="lazy" alt="" class="in-mach">
+    <div class="block-mach-4 second">
+        <div class="teg-mach-xr">
+            <img src="https://cdn.prod.website-files.com/64aceaf9cb10fc1c8e4efdbc/66320c05ac2b58c3877b7aec_Group%201948758574.svg" loading="lazy" alt="" class="icon-teg ochki">
+            <div class="text-29">XR</div>
+        </div>
+        <div class="box-im-mach-3">
+            <img src="https://cdn.prod.website-files.com/64aceaf9cb10fc1c8e4efdbc/66320b9a11ed170344e75fdb_Frame%201948759401.webp" loading="lazy" alt="" class="i-4n-mach">
+        </div>
     </div>
+    <div class="block-mach-2 third">
+        <div class="box-im-mach-2">
+            <img src="https://cdn.prod.website-files.com/64aceaf9cb10fc1c8e4efdbc/66320b66a3f96d85f32c61f0_Group%201948758576%20(1).webp" loading="lazy" alt="" class="in-mach-2">
+        </div>
+        <div class="teg-mach-smart">
+            <img src="https://cdn.prod.website-files.com/64aceaf9cb10fc1c8e4efdbc/66320c0cdbdcfd73b6f0b608_Group%201948758575%20(1).svg" loading="lazy" alt="" class="icon-teg">
+            <div class="text-29">Smartwatches</div>
+        </div>
+    </div>
+    <div class="block-mach-3 forth">
+        <div class="teg-mach-smart app">
+            <div class="text-29">Mobile App</div>
+            <img src="https://cdn.prod.website-files.com/64aceaf9cb10fc1c8e4efdbc/66320c0a46af4dde082831f6_Group%201948758575.svg" loading="lazy" alt="" class="icon-teg">
+        </div>
+        <div class="box-im-mach-3">
+            <img src="https://cdn.prod.website-files.com/64aceaf9cb10fc1c8e4efdbc/66320b93a3f96d85f32c7886_Group%201948758719.webp" loading="lazy" alt="" class="in-mach-3">
+        </div>
     </div>
   </div>
 
